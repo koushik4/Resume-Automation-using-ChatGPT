55da69e298110fbd6a27851f251c1e1f200e6401
srinivaskoushik <srinivas.koushik4@gmail.com>
2023-01-09 17:46:25 -0500

    Added functions to get resume points using ChatGPT

---
+++ gpt_summary.py
@@ -0,0 +1,23 @@
+import openai
+
+openai.api_key = "YOUR OPENAI KEY"
+
+
+def get_summary(description):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Summarize the following in bullet points for my resume \n\n"+description,
+        temperature=0,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    if len(response['choices'][0]) > 0:
+        points = response['choices'][0]['text'].split("\n")
+        print(points)
+        s = ""
+        for point in points:
+            s += point[1:]+"\n"
+        return s
+    return ""
