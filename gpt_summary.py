72c73c6c6936667b8adee15d7d5d0d1a83fe71f8
srinivaskoushik <srinivas.koushik4@gmail.com>
2023-01-09 22:32:16 -0500

    Changed GPT description prediction to 2 bullet points for Projects

--- gpt_summary.py
+++ gpt_summary.py
@@ -2,8 +2,7 @@ import openai
 
 openai.api_key = "YOUR OPENAI KEY"
 
-
-def get_summary(description):
+def get_summary_for_resume(description):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt="Summarize the following in bullet points for my resume \n\n"+description,
@@ -15,8 +14,27 @@ def get_summary(description):
     )
     if len(response['choices'][0]) > 0:
         points = response['choices'][0]['text'].split("\n")
-        print(points)
         s = ""
+        # Remove the Bullet point from the response text
+        for point in points:
+            s += point[1:]+"\n"
+        return s
+    return ""
+
+def get_summary_for_projects(description):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Summarize the following in 2 bullet points for my resume \n\n"+description,
+        temperature=0,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    if len(response['choices'][0]) > 0:
+        points = response['choices'][0]['text'].split("\n")
+        s = ""
+        # Remove the Bullet point from the response text
         for point in points:
             s += point[1:]+"\n"
         return s
